6549e6c90b0cc70e1d756211ecbed10df368312e
Nienke Dekker <nienkedekker@gmail.com>
2023-05-12 13:25:23 +0200

    No longer allow optional results in tests

--- daily-precall-react/src/types.ts
+++ daily-precall-react/src/types.ts
@@ -2,28 +2,15 @@ import {
 	DailyFatalErrorType,
 	DailyNonFatalErrorType,
 } from '@daily-co/daily-js';
-import { ConnectionModes } from './utils/constants.js';
 
 export interface RTCPeerConnectionWithBuffers extends RTCPeerConnection {
 	bufferedIceCandidates?: RTCIceCandidate[] | null;
 	iceCandidates?: RTCIceCandidate[] | null;
 }
 
-export interface IceServerInterface {
-	urls?: string;
-	url?: string;
-	[key: number]: {
-		url?: string;
-		urls?: string;
-		credential?: string;
-		username?: string;
-	};
-}
 export type RTCStatsReportStat =
 	| RTCIceCandidatePairStats[]
 	| RTCInboundRtpStreamStats;
-
-export type ResultTypes = 'bad' | 'good' | 'warning' | 'failed' | '';
 
 export interface Throughput {
 	maxRTT: number | null;
@@ -32,28 +19,26 @@ export interface Throughput {
 
 export interface ConnectionTestReport {
 	errors?: ErrorEvent[];
-	id?: string;
-	result?: ResultTypes;
-	startedAt?: Date;
-	throughput?: Throughput | null;
+	id: string;
+	result: 'bad' | 'good' | 'warning' | 'failed' | '';
+	startedAt: Date;
+	throughput: Throughput | null;
 }
 
 export interface NetworkTestReport {
-	connected?: ConnectionModes[];
 	errors?: ErrorEvent[];
-	failed?: ConnectionModes[];
-	id?: string;
-	result?: 'failed' | 'warning' | 'passed' | '';
-	startedAt?: Date;
+	id: string;
+	result: 'connected' | 'failed' | '';
+	startedAt: Date;
 }
 
 export interface WebsocketsTestReport {
 	errors?: ErrorEvent[];
-	failed?: string[];
-	id?: string;
-	passed?: string[];
-	result?: string | 'passed' | 'failed' | 'warning' | '';
-	startedAt?: Date;
+	failed: string[];
+	id: string;
+	passed: string[];
+	result: string | 'passed' | 'failed' | 'warning' | '';
+	startedAt: Date;
 }
 
 export declare type ErrorEvent = {
